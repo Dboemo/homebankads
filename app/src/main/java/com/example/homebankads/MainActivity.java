b8ccdf430290e2c6f6d75b6b90de183c07bd4343
Daniel Boemo <daniel.boemo@iffarroupilha.edu.br>
2024-10-08 17:59:34 -0300

    primeiro commit

---
+++ app/src/main/java/com/example/homebankads/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.homebankads;
+
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    Button btlogar;
+    TextView linkregistro;
+    EditText edtlogin,edtsenha;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        this.setTitle("Home Bank ADS");
+        btlogar=(Button) findViewById(R.id.buttonlogar);
+        edtlogin=(EditText)findViewById(R.id.edtemail);
+        edtsenha=(EditText)findViewById(R.id.edtsenha);
+        btlogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                usuariocontrole usrcontrol = new usuariocontrole(MainActivity.this);
+                if(usrcontrol.checkusuario(edtlogin.getText().toString(),
+                        edtsenha.getText().toString())){
+                Intent i = new Intent(getApplicationContext(), telaprincipal.class);
+                i.putExtra("login",edtlogin.getText().toString() );
+                i.putExtra("senha", edtsenha.getText().toString());
+                startActivity(i);
+                }
+            }
+        });
+        linkregistro =(TextView) findViewById(R.id.linkCriaConta);
+        linkregistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), telaregistro.class);
+
+                startActivity(i);
+            }
+        });
+
+
+    }
+}
---
+++ app/src/main/java/com/example/homebankads/conectadb.java
@@ -0,0 +1,42 @@
+package com.example.homebankads;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class conectadb extends SQLiteOpenHelper {
+
+    String tab_usr = "CREATE TABLE IF NOT EXISTS usuario (" +
+            "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+            "login TEXT," +
+            "senha TEXT," +
+            "saldo NUMERIC)";
+    String tab_mov ="CREATE TABLE IF NOT EXISTS movimento (" +
+            "idmov INTEGER  NOT NULL PRIMARY KEY AUTOINCREMENT, "+
+            "idusr INTEGER ," +
+            "tipo_opera TEXT ," +
+            "valor NUMERIC ," +
+            "destino TEXT  ," +
+            "dataopera DATE )";
+
+    conectadb(Context contexto) {
+
+        super(contexto, "homebankdb", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+        sqLiteDatabase.execSQL(tab_usr);
+        sqLiteDatabase.execSQL(tab_mov);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        String RECRIA = "DROP TABLE IF EXISTS usuario";
+        String RECRIA2 = "DROP TABLE IF EXISTS movimento";
+        sqLiteDatabase.execSQL(RECRIA);
+        sqLiteDatabase.execSQL(RECRIA2);
+        onCreate(sqLiteDatabase);
+    }
+}
